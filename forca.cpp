4bf073107714b8e6e8a12ce70462530fb4162320
Lucas Vale <lucasvale0904@gmail.com>
2025-01-11 02:03:28 -0300

    V 0.6

--- forca.cpp
+++ forca.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 using namespace std;
 
 const string PALAVRA_SECRETA = "MELANCIA";
 map<char, bool> chutou;
-
+vector<char> chutes_errados;
 bool letra_existe(char chute){
     /*for(int i = 0; i < PALAVRA_SECRETA.size(); i++){
         if (chute == PALAVRA_SECRETA[i]){
@@ -26,31 +27,41 @@ bool letra_existe(char chute){
 
 int main () {
     
-    cout << PALAVRA_SECRETA << endl;
-
+    cout << "*****************************************" << endl;
+    cout << "*******Bem vindos ao jogo da forca*******" << endl;
+    cout << "*****************************************" << endl << endl;
+    
+    
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
     while (nao_acertou && nao_enforcou){//continua se não acertou ainda ou ainda não acabou as tentativas 
+        
+        cout << "Chutes errados: ";
+        for (char letra : chutes_errados) {
+            cout << letra << " ";
+        }
+        cout << endl;
         for (char letra : PALAVRA_SECRETA){
             if (chutou[letra]){
                 cout << letra << " ";
             }else{
                 cout << "_ ";
             }
-        }
+        } 
         cout << endl;
+        cout << "Tente uma letra: ";
         char chute;
         cin >> chute;
-
+ 
         chutou[chute] = true;
 
 
         if(letra_existe(chute)){
-            cout << "Voce acertou! Seu chute esta na palavra." << endl;
+            cout << endl<< "Voce acertou! Seu chute esta na palavra." << endl << endl;
         } else{
-            cout << "Voce errou! Seu chute nao esta na palavra." << endl;
+            cout << endl<< "Voce errou! Seu chute nao esta na palavra." << endl << endl;
+            chutes_errados.push_back(chute);
         }
     }
-    
-}
+}   
