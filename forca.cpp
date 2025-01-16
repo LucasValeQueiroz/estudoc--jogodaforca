da6098d3d44d6ff82b4b0b8fe6aea4e297373e00
Lucas Vale <lucasvale0904@gmail.com>
2025-01-16 08:45:33 -0300

    V 0.8

--- forca.cpp
+++ forca.cpp
@@ -2,20 +2,23 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-const string PALAVRA_SECRETA = "MELANCIA";
+string palavra_secreta = "MELANCIA";
 map<char, bool> chutou;
 vector<char> chutes_errados;
 bool letra_existe(char chute){
-    /*for(int i = 0; i < PALAVRA_SECRETA.size(); i++){
-        if (chute == PALAVRA_SECRETA[i]){
+    /*for(int i = 0; i < palavra_secreta.size(); i++){
+        if (chute == palavra_secreta[i]){
             return true;
         }
     }
 
 *///jeito de fazer em C++ e C porem no C++ 11 fizeram um novo jeito como o que vem a seguir
-    for(char letra : PALAVRA_SECRETA){
+    for(char letra : palavra_secreta){
         if (chute == letra){
             return true;
         }
@@ -24,7 +27,7 @@ bool letra_existe(char chute){
 }
 
 bool nao_acertou(){
-    for(char letra : PALAVRA_SECRETA){
+    for(char letra : palavra_secreta){
         if (!chutou[letra]) {
             return true;
         }
@@ -54,7 +57,7 @@ void imprime_erros(){
 
 void imprime_palavra(){
 
-    for (char letra : PALAVRA_SECRETA){
+    for (char letra : palavra_secreta){
         if (chutou[letra]){
             cout << letra << " ";
         }else{
@@ -79,8 +82,51 @@ void chute_usuario(){
             chutes_errados.push_back(chute);
         }
 }
+
+vector<string> le_arquivo(){
+    ifstream arquivo;
+    arquivo.open("palavra.txt");
+
+    if (!arquivo){
+        cout << "Arquivo nao encontrado!" << endl;
+    }else {
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
+        
+        cout << "O arquivo possui " << quantidade_palavras << " palavras." << endl;
+
+        vector<string> palavras_do_arquivo;
+        int escolha;
+        cout << "Quer ver as palavras da forca: "<<endl<<"(1)SIM (2)NAO : " ;
+        cin >> escolha;
+        for(int i = 0; quantidade_palavras > i; i++){
+            string palavra_lida;
+            arquivo >> palavra_lida;
+            if (escolha == 1){
+                cout <<"Na linha " << i+1 <<" : " << palavra_lida << endl;
+            }
+            
+            
+            palavras_do_arquivo.push_back(palavra_lida);
+        }
+        return palavras_do_arquivo;
+    }
+           
+    arquivo.close();
+}
+
+void sorteia_palavra(){
+    vector<string> palavras = le_arquivo();
+
+    srand(time(NULL));
+    int indice_sorteado = rand() % palavras.size();
+    palavra_secreta = palavras[indice_sorteado];
+}
+
 int main () {
     imprime_cabecalho();
+
+    sorteia_palavra();
 
     while (nao_acertou() && nao_enforcou()){//continua se não acertou ainda ou ainda não acabou as tentativas 
         
@@ -91,7 +137,7 @@ int main () {
         chute_usuario();
     }
 
-    cout << "Fim de jogo!" <<endl << "A palavra secreta era: "<< PALAVRA_SECRETA<< endl;
+    cout << "Fim de jogo!" <<endl << "A palavra secreta era: "<< palavra_secreta<< endl;
     if (nao_acertou()){
         cout << "Voce perdeu!" << endl;
     } else{
